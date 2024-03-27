6a1b31a859958047662527ce1bb949fff93b9e61
anasou <anasounatural@gmail.com>
2024-03-27 16:52:04 -0300

    Pre Branches

--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,22 @@
 // Para incluir los diferentes sets de cartas podemos _importar_ el archivo
 // JavasSript que contenga el `export` correspondiente...
 //
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+//import baralho from '../data/webdev/webdev.js';
+//console.log(baralho);
+//export default App;
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
 //
-// fetch('./data/pokemon/pokemon.json')
+// fetch('./data/webdev/webdev.json')
 //   .then(resp => resp.json())
 //   .then(console.log)
 //   .catch(console.error);
-//
 
-const App = () => {
-  const el = document.createElement('div');
 
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
 
-  return el;
-};
 
-export default App;
+
+
+
+
